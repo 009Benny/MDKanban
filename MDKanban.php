8808fb043c47fe92fc40b15f9f061863d7587e44
Benny <009bennyreyes@gmail.com>
2019-01-30 15:10:25 -0600

    Plugin para adminstrar personal y proyectos

---
+++ MDKanban.php
@@ -0,0 +1,27 @@
+<?php
+/*
+* MDKanban
+*
+* @link              https://milenio.com/
+* @since             1.0.0
+* @package           MDKanban
+*
+* @MDKanban
+* Plugin Name:       MDKanban
+* Plugin URI:        https://podz.io/agilepress/
+* Description:       AgilePress brings Agile product/project management to WordPress.
+* Version:           1.608.2
+* Author:            KMD Enterprises, LLC
+* Author URI:        https://kmd.enterprises/
+* License:           GPL-2.0+
+* License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+* Text Domain:       agilepress
+* Domain Path:       /languages
+*/
+
+
+
+
+
+
+?>
